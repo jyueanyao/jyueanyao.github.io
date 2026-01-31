3a914077c411c4501e7e27400dfd93280ab3e3f6
Julia Yao <144055202+julia123123@users.noreply.github.com>
2026-01-30 20:40:30 -0800

    Linkedin Link
    Linkedin Link

--- components/navbar.js
+++ components/navbar.js
@@ -7,7 +7,7 @@ const createNavBar = () => {
       </a>
       <nav class="nav__links" aria-label="Main navigation">
         <a href="https://drive.google.com/file/d/1p9JfSayPRHn9ztjnRlRQqekNmAJl0Klr/view?usp=drive_link" target="_blank" rel="noopener">Resume</a>
-        <a href="https://www.linkedin.com/in/jyuean" target="_blank" rel="noopener">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/juliayao333/" target="_blank" rel="noopener">LinkedIn</a>
       </nav>
     </div>
   `;
